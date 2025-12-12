a0a39520b22b6cf147837b50cefdf99954b4e3c8
swapnilgole0712 <swapnilgole0712@gmail.com>
2025-12-12 18:57:46 +0530

    add javascript

---
+++ Day 2/script.js
@@ -0,0 +1,30 @@
+
+// console.log("i am swapnil gole")
+// console.warn("i am swapnil gole")
+// console.error("i am swapnil gole")
+// console.clear()
+
+
+// let a=2
+// let b=3
+// let sum= 2+3
+// console.log (result)
+
+
+// let a = 10;
+// a += 2;
+
+// let a = 10
+// a -= 2
+
+// let a = 10
+// a *= 5
+
+// let a = 10
+// a /= 3
+
+let a = 10
+a  %= 2
+
+
+console.log(a);
